7500c463f18789573b96e456f850c1ed45bba9fc
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-04-30 21:24:48 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryImage = document.querySelector(".gallery");
+
+function addGallery(galleryItems) {
+  const markup = galleryItems.map(img =>
+`<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`).join('');
+
+
+//   galleryImage.insertAdjacentHTML("afterbegin", galleryItemsString);
+
+galleryImage.addEventListener("click", onGalleryClick);
+
+function onGalleryClick (event) {
+  event.preventDefault();
+  
+//   const filterDatasetImg = event.target.dataset.source;
+//   if (!filterDatasetImg) return;
+
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}">
+`, {
+
+      onShow: instance => console.log('onShow', instance.element()),
+      onClose: instance => console.log('onClose', instance.element())
+  });
+  instance.show();
+
+  galleryImage.addEventListener("keydown", handlerRemove);
+  function handlerRemove (event) {
+      if (event.code === "Escape") {
+          instance.close(() => galleryImage.removeEventListener("keydowb", handlerRemove))};
+
+
+
+
+
+// console.log(galleryItems);
